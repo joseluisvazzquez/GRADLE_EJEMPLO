c20a0cb49b956c8773cb61671cec34357550c9b6
joseluisvazzquez <jvazquez3@nigul.cide.es>
2024-05-02 20:05:59 +0200

    Last-Test

--- GRADLEPROYECT/app/src/test/java/gradleproyect/AppTest.java
+++ GRADLEPROYECT/app/src/test/java/gradleproyect/AppTest.java
@@ -5,40 +5,45 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+
 
 
 public class AppTest {
     private Habitaciones habitaciones;
     private Usuarios usuarios;
-    private Registro registros;
+    private Registres registres;
 
     @BeforeEach
     public void setUp() {
         habitaciones = new Habitaciones(new ArrayList<>());
         usuarios = new Usuarios(new ArrayList<>());
-        registros = new Registro(new HashMap<>());
+        registres = new Registres();
         
     }
     @Test
-    public void testFerReserva(){
-        UsuarioDto usuario1 = new UsuarioDto("user1", "user1@gmail.com");
-        Habitacion habitacion1 = new Habitacion(2L, "Suite", 300.0);
-        Habitacion habitacion2 = new Habitacion(4L, "Suite Presidencial", 300.0);
-        registros.fer_reserva(habitacion1, usuario1);
-        registros.fer_reserva(habitacion2, usuario1);
-
-        assertEquals(habitacion1, registros.visualitzar_reservas(usuario1));
-
-    }
-    @Test
-    public void testVisualitzarReseva(){
-        usuarios.CrearUsuario("User", "User@gmail.com");
-        habitaciones.registrar_habitacio(6L, "Suite", 8000.0);
-        assertEquals(registros, habitaciones);
-
+    public void testFerReserva() {
+        UsuarioDto usuario1 = usuarios.CrearUsuario("user1", "user1@gmail.com");
+        Habitacion habitacion1 = habitaciones.registrar_habitacio(1L, "Single", 100.0);
         
+        Registre registre1 = registres.fer_reserva(habitacion1.getId(), usuario1);
+
+        assertNotNull(registre1);
+        assertEquals(habitacion1.getId(), registre1.getNumero_habitacio());
+        assertEquals(usuario1, registre1.getUsuari());
     }
+
+    @Test
+    public void testVisualitzaReserves() {
+        UsuarioDto usuario1 = usuarios.CrearUsuario("user1", "user1@gmail.com");
+        Habitacion habitacion1 = habitaciones.registrar_habitacio(1L, "Single", 100.0);
+        Registre reserva1 = registres.fer_reserva(habitacion1.getId(), usuario1);
+
+        ArrayList<Registre> reservasUsuario1 = registres.visualitza_reserves(usuario1.getNombre());
+
+        assertNotNull(reservasUsuario1);
+        assertTrue(reservasUsuario1.contains(reserva1));
+    }
+
     @Test
     public void testEliminarUsuario(){
         usuarios.CrearUsuario("user2", "user2@gmail.com");
